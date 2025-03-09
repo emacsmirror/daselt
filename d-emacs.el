68ec25393485fe99ca8604f6370df6b653e2050a
Alexander Prähauser <ahprae@protonmail.com>
2025-03-09 14:51:56 +0100

    Added d-emacs.el

---
+++ d-emacs.el
@@ -0,0 +1,36 @@
+;;; d-emacs.el --- Load d-emacs-files  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025  Alexander Prähauser
+
+;; Author: Alexander Prähauser <ahprae@protonmail.com>
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(require 'd-emacs-base)
+(require 'd-emacs-coords)
+(require 'd-emacs-xkb)
+(require 'd-emacs-dfk)
+(require 'd-emacs-bind)
+(require 'd-emacs-dirs)
+(require 'd-emacs-mode)
+
+(provide 'd-emacs)
+;;; d-emacs.el ends here
